0dd6a7e1c83eed6bb1d5211fbc9f69af4b5b4d22
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2020-03-08 21:42:41 +0100

    Add basic styles for login form

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,31 +9,37 @@ const Home = props => {
     setActiveTab(!activeTab);
   };
   return (
-    <div className="card">
-      <div className="btn-container navbar">
-        <button
-          onClick={handleClick}
-          className="btn tab nav-item"
-          data-toggle="tab"
-          aria-controls="register"
-        >
-          register
-        </button>
-        <button
-          onClick={handleClick}
-          className="btn tab nav-item"
-          data-toggle="tab"
-          aria-controls="login"
-        >
-          login
-        </button>
+    <div className="homepage">
+      <div className="homepage-form card">
+        <div className="btn-container navbar">
+          <button
+            onClick={handleClick}
+            className="btn tab nav-item"
+            data-toggle="tab"
+            aria-controls="register"
+          >
+            register
+          </button>
+          <button
+            onClick={handleClick}
+            className="btn tab nav-item"
+            data-toggle="tab"
+            aria-controls="login"
+          >
+            login
+          </button>
+        </div>
+        <Register
+          className="formWrapper"
+          type="register"
+          isActive={!activeTab}
+        ></Register>
+        <Login
+          className="formWrapper"
+          type="login"
+          isActive={activeTab}
+        ></Login>
       </div>
-      <Register
-        className="formWrapper"
-        type="register"
-        isActive={!activeTab}
-      ></Register>
-      <Login className="formWrapper" type="login" isActive={activeTab}></Login>
     </div>
   );
 };
